91662daecbd5979b3d405585130e143b4834bade
Sanju <sanjupipal@gmail.com>
2021-02-08 08:28:46 +0530

    final

--- domainReport.js
+++ domainReport.js
@@ -9,10 +9,12 @@ const DomainReport = () => {
   };
 
   const minuteMap = {};
+  const fiveMinuteMap = {};
+
   let currLogMinute = null;
-  eventSource.onmessage = function (event) {
+  let currLogFiveMinute = null;
+  function oneMinuteHandler(event, minute) {
     minute = new Date().getMinutes();
-    minute = minute ? minute : 60;
     if (!currLogMinute) {
       currLogMinute = minute;
     }
@@ -28,6 +30,29 @@ const DomainReport = () => {
       page.add(event.page_id);
       domain.set(event.meta.domain, page);
     }
+  }
+  function fiveMinuteHandler(event, minute) {
+    minute = Math.floor(minute / 5);
+    if (!currLogFiveMinute) {
+      currLogFiveMinute = minute;
+    }
+    if (!fiveMinuteMap[minute]) {
+      fiveMinuteMap[minute] = new Map();
+    }
+    domain = fiveMinuteMap[minute];
+    event = JSON.parse(event.data);
+    if (domain.has(event.meta.domain)) {
+      domain.get(event.meta.domain).add(event.page_id);
+    } else {
+      let page = new Set();
+      page.add(event.page_id);
+      domain.set(event.meta.domain, page);
+    }
+  }
+  eventSource.onmessage = (event) => {
+    minute = new Date().getMinutes();
+    oneMinuteHandler(event, minute);
+    fiveMinuteHandler(event, minute);
   };
 
   const generateOneMinReport = () => {
@@ -48,16 +73,53 @@ const DomainReport = () => {
     delete minuteMap[currLogMinute];
     currLogMinute += 1;
     if (currLogMinute == 60) {
-      currLogMinute = 1;
+      currLogMinute = 0;
+    }
+  };
+
+  const generateFiveMinReport = () => {
+    domain = fiveMinuteMap[currLogFiveMinute];
+    let report = new Map();
+    domain.forEach((k, v) => {
+      report.set(v, k.size);
+    });
+    domain.clear();
+    const mapSort1 = new Map([...report.entries()].sort((a, b) => b[1] - a[1]));
+    let totalDomainUpdated = mapSort1.size;
+    console.log(
+      `total number of Wikipedia Domain Updated : ${totalDomainUpdated}`
+    );
+    mapSort1.forEach((k, v) => {
+      console.log(`${v} : ${k} pages updated`);
+    });
+    delete fiveMinuteMap[currLogFiveMinute];
+    currLogFiveMinute += 5;
+    if (currLogFiveMinute == 60) {
+      currLogFiveMinute = 0;
     }
   };
 
   setInterval(() => {
+    minute = new Date().getMinutes();
+    if (minute == currLogMinute) {
+      return;
+    }
     console.log(
       "\n ============================ One Min Domain Report ============================ \n"
     );
     generateOneMinReport();
-  }, 60000);
+  }, 60 * 1000);
+
+  setInterval(() => {
+    minute = Math.floor(new Date().getMinutes() / 5);
+    if (minute == currLogFiveMinute) {
+      return;
+    }
+    console.log(
+      "\n ============================ Five Min Domain Report ============================ \n"
+    );
+    generateFiveMinReport();
+  }, 5 * 60 * 1000);
 };
 
 module.exports = DomainReport;
--- userReport.js
+++ userReport.js
@@ -8,19 +8,19 @@ const UserReport = () => {
     console.error("--- Encountered error", event);
   };
 
-  const minutesMap = {};
-  let currLogMinute = null;
-  eventSource.onmessage = function (event) {
-    event = JSON.parse(event.data);
-    minute = new Date().getMinutes();
-    minute = minute ? minute : 60;
-    if (!currLogMinute) {
-      currLogMinute = minute;
+  const oneMinuteMap = {};
+  let currLogForOneMinute = null;
+  const fiveMinuteMap = {};
+  let currLogForFiveMinute = null;
+
+  const handleUserOneMinReport = (event, minute) => {
+    if (!currLogForOneMinute) {
+      currLogForOneMinute = minute;
     }
-    if (!minutesMap[minute]) {
-      minutesMap[minute] = new Map();
+    if (!oneMinuteMap[minute]) {
+      oneMinuteMap[minute] = new Map();
     }
-    user = minutesMap[minute];
+    user = oneMinuteMap[minute];
     if (
       event.meta.domain == "en.wikipedia.org" &&
       !event.performer.user_is_bot
@@ -38,27 +38,81 @@ const UserReport = () => {
     }
   };
 
+  const handleUserFiveMinReport = (event, minute) => {
+    minute = Math.floor(minute / 5);
+    if (!currLogForFiveMinute) {
+      currLogForFiveMinute = minute;
+    }
+    if (!fiveMinuteMap[minute]) {
+      fiveMinuteMap[minute] = new Map();
+    }
+    user = fiveMinuteMap[minute];
+    if (
+      event.meta.domain == "en.wikipedia.org" &&
+      !event.performer.user_is_bot
+    ) {
+      if (user.has(event.performer.user_text)) {
+        user[event.performer.user_text] = Math.max(
+          user.get(event.performer.user_text),
+          event.performer.user_edit_count
+        );
+      } else {
+        if (event.performer.user_edit_count) {
+          user.set(event.performer.user_text, event.performer.user_edit_count);
+        }
+      }
+    }
+  };
+
+  eventSource.onmessage = (event) => {
+    event = JSON.parse(event.data);
+    minute = new Date().getMinutes();
+    handleUserOneMinReport(event, minute);
+    handleUserFiveMinReport(event, minute);
+  };
+
   const generateOneMinReport = () => {
-    user = minutesMap[currLogMinute];
+    user = oneMinuteMap[currLogForOneMinute];
     const mapSort1 = new Map([...user.entries()].sort((a, b) => b[1] - a[1]));
     user.clear();
     console.log("Users who made changes to en.wikipedia.org");
     mapSort1.forEach((k, v) => {
       console.log(`${v} : ${k}`);
     });
-    delete minutesMap[currLogMinute];
-    currLogMinute += 1;
-    if (currLogMinute == 60) {
-      currLogMinute = 1;
+    delete oneMinuteMap[currLogForOneMinute];
+    currLogForOneMinute += 1;
+    if (currLogForOneMinute == 60) {
+      currLogForOneMinute = 0;
     }
   };
 
+  const generateFiveMinReport = () => {
+    user = fiveMinuteMap[currLogForFiveMinute];
+    const mapSort1 = new Map([...user.entries()].sort((a, b) => b[1] - a[1]));
+    user.clear();
+    console.log("Users who made changes to en.wikipedia.org");
+    mapSort1.forEach((k, v) => {
+      console.log(`${v} : ${k}`);
+    });
+    delete fiveMinuteMap[currLogForFiveMinute];
+    currLogForFiveMinute += 1;
+    if (currLogForFiveMinute == 60) {
+      currLogForFiveMinute = 0;
+    }
+  };
   setInterval(() => {
     console.log(
       "\n ============================ One Min User Report ============================ \n"
     );
     generateOneMinReport();
-  }, 60000);
+  }, 60 * 1000);
+
+  setInterval(() => {
+    console.log(
+      "\n ============================ Five Min User Report ============================ \n"
+    );
+    generateFiveMinReport();
+  }, 5 * 60 * 1000);
 };
 
 module.exports = UserReport;
